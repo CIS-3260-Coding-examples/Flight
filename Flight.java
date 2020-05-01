aa1efab339e2a29eedfdcb7a0b1e5356659cb60d
junghooona <61996952+junghooona@users.noreply.github.com>
2020-04-30 20:32:06 -0400

    Add files via upload

---
+++ Flight.java
@@ -0,0 +1,68 @@
+package Classes;
+
+import java.util.ArrayList;
+
+public class Flight {
+	private String flightId;
+	private String dateCreated;
+	private String cityName ;
+	private String destinantionCity;
+	private String timeIn;
+	private String timeOut;
+	private static ArrayList<Flight> newflight;
+	
+	
+	public Flight() {
+		this.flightId = "";
+		this.dateCreated =  "";
+		this.cityName = "";
+		this.destinantionCity = "";
+		this.timeIn ="";
+		this.timeOut ="";
+	}
+	public Flight(String flightId ,String dateCreated, String cityName, String destinantionCity,String timeIn, String timeOut) {
+		this.flightId = flightId;
+		this.dateCreated = dateCreated;
+		this.cityName = cityName;
+		this.destinantionCity = destinantionCity;
+		this.timeIn = timeIn;
+		this.timeOut = timeOut;
+
+	}
+	public String getFlightId() {
+		return flightId;
+	}
+	public void setFlightId(String flightId) {
+		this.flightId = flightId;
+	}
+	public String getDateCreated() {
+		return dateCreated;
+	}
+	public void setDateCreated(String dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+	public String getCityName() {
+		return cityName;
+	}
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
+	public String getDestinantionCity() {
+		return destinantionCity;
+	}
+	public void setDestinantionCity(String destinantionCity) {
+		this.destinantionCity = destinantionCity;
+	}
+	public String getTimeIn() {
+		return timeIn;
+	}
+	public void setTimeIn(String timeIn) {
+		this.timeIn = timeIn;
+	}
+	public String getTimeOut() {
+		return timeOut;
+	}
+	public void setTimeOut(String timeOut) {
+		this.timeOut = timeOut;
+	}
+}
